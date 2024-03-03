8bff6196818aa917c23dc7aaf36b6c0bec715491
Zitouna11 <161982124+Zitouna11@users.noreply.github.com>
2024-03-03 01:11:51 +0100

    Update config.js

--- config.js
+++ config.js
@@ -10,15 +10,15 @@ import moment from 'moment-timezone';
 global.botnumber = ""
 
 global.owner = [
-  ['212717457920', 'bobiza', true],
+  ['212772554870', 'bobiza', true],
 
-  ['212717457920','bobiza', true],
-  ['212717457920'],
-  ['212717457920']
+  ['212772554870','bobiza', true],
+  ['21272554870'],
+  ['212772554870']
 ];
 
-global.suittag = ['212717457920'];
-global.prems = ['212717457920'];
+global.suittag = ['212772554870'];
+global.prems = ['212772554870'];
 
 global.packname = 'bobiza bot';
 global.author = 'bobiza';
